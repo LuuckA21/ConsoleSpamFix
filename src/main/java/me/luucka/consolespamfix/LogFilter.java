523a93b064d950ecb9753454f9ed3ca9e7d2ee00
LuuckA <luca.dalu@outlook.it>
2023-06-20 13:03:32 +0200

    1.0.5

--- src/main/java/me/luucka/consolespamfix/LogFilter.java
+++ src/main/java/me/luucka/consolespamfix/LogFilter.java
@@ -8,6 +8,8 @@ import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.message.Message;
 
+import java.util.List;
+
 public class LogFilter implements Filter {
 
     private final ConsoleSpamFixPlugin plugin;
@@ -17,7 +19,8 @@ public class LogFilter implements Filter {
     }
 
     public Filter.Result checkMessage(String message) {
-        for (final String msg : plugin.getConfig().getStringList("messages-to-hide")) {
+        final List<String> messagesToHide = plugin.getConfig().getStringList("messages-to-hide");
+        for (String msg : messagesToHide) {
             if (message.toLowerCase().contains(msg.toLowerCase())) return Result.DENY;
         }
         return Result.NEUTRAL;
